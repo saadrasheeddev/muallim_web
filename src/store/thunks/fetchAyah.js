7f4fe8cbda3027e0c71b0605a8727b57e1f8a25d
Saad Rasheed <84860156+saadrasheeddev@users.noreply.github.com>
2024-01-23 11:48:22 +0500

    Update fetchAyah.js

--- src/store/thunks/fetchAyah.js
+++ src/store/thunks/fetchAyah.js
@@ -2,7 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 const fetchAyah = createAsyncThunk('ayah/fetch', async () => {
-  const response = await axios.get('https://muallim-web.vercel.app/ayah');
+  const response = await axios.get('https://muallim.vercel.app/api/fetchAyah');
 
   return response.data;
 });
